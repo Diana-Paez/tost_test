3ae27a87bbbe77c7cf640f43b09d3f6e825560e0
Diana Paez <developermerci@gmail.com>
2024-11-07 08:18:11 -0500

    Configure app locator and register services

---
+++ lib/app/app.locator.dart
@@ -0,0 +1,12 @@
+import 'package:get_it/get_it.dart';
+
+import 'package:flutter_test_tots/app/core/services/auth_service.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+final locator = GetIt.instance;
+
+void setupLocator() {
+  locator.registerLazySingleton(() => AuthService());
+  locator.registerLazySingleton(
+      () => NavigationService()); // Registra el servicio de navegación
+}
