2c8d645b8315110385fbb4ad0b36871c88e632eb
Diana Paez <developermerci@gmail.com>
2024-11-07 21:37:07 -0500

    Add token storage and retrieval in AuthService

--- lib/app/core/services/auth_service.dart
+++ lib/app/core/services/auth_service.dart
@@ -2,13 +2,18 @@ import 'dart:convert';
 import 'dart:developer';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
-import '../models/auth_response.dart';
+import '../models/auth_response_model.dart';
 
 class AuthService {
+  String? _token; // Token almacenado
+
+  // Método para obtener el token
+  String? get token => _token;
+
   final String _baseUrl =
       'https://myback-execute-dot-my-back-401316.uc.r.appspot.com/6-tots-test/oauth/token';
 
-  Future<AuthResponse?> authenticate(String email, String password) async {
+  Future<AuthResponseModel?> authenticate(String email, String password) async {
     final Map<String, String> headers = {'Content-Type': 'application/json'};
     final String body = json.encode({"email": email, "password": password});
 
@@ -20,8 +25,12 @@ class AuthService {
       );
 
       if (response.statusCode >= 200 && response.statusCode <= 299) {
+        final AuthResponseModel authResponseModel =
+            AuthResponseModel.fromJson(json.decode(response.body));
+
+        _token = authResponseModel.accessToken;
         // Convertimos la respuesta en un AuthResponse
-        return AuthResponse.fromJson(json.decode(response.body));
+        return authResponseModel;
       } else {
         log('Error de autenticación: ${response.reasonPhrase}');
         return null;
