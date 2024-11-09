e6f38d06c3e9c851ec7e66a603094321d15bc279
Diana Paez <developermerci@gmail.com>
2024-11-09 07:40:53 -0500

    Add request to add new client

--- lib/app/core/services/clients_service.dart
+++ lib/app/core/services/clients_service.dart
@@ -20,10 +20,51 @@ class ClientsService {
     if (response.statusCode == 200) {
       Map<String, dynamic> responseBody = json.decode(response.body);
       List data = responseBody['data'];
-      //TODO
+
       return data.map((client) => Client.fromJson(client)).toList();
     } else {
       throw Exception('Failed to load clients');
+    }
+  }
+
+  // Método para agregar un nuevo cliente
+  Future<bool?> addClient({
+    required String token,
+    required String firstName,
+    required String lastName,
+    required String email,
+    String? photo,
+  }) async {
+    final Uri url = Uri.parse('$baseUrl/clients');
+
+    // Headers con el token de autorización
+    final Map<String, String> headers = {
+      'Authorization': 'Bearer $token',
+      'Content-Type': 'application/json'
+    };
+
+    // Cuerpo de la solicitud en formato JSON
+    final String body = jsonEncode({
+      "firstname": firstName,
+      "lastname": lastName,
+      "email": email,
+      "address": "",
+      "photo": photo ?? "",
+      "caption": "",
+    });
+
+    // Realiza la petición POST
+    final response = await http.post(
+      url,
+      headers: headers,
+      body: body,
+    );
+
+    // Verifica si la petición fue exitosa
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return true;
+    } else {
+      throw Exception('Failed to add client');
     }
   }
 }
