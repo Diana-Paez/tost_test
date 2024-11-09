4c4dded12b95ab523032795acc0bcfb40938cfbd
Diana Paez <developermerci@gmail.com>
2024-11-09 07:42:56 -0500

    Implement clients search

--- lib/ui/viewmodels/clients_viewmodel.dart
+++ lib/ui/viewmodels/clients_viewmodel.dart
@@ -5,9 +5,22 @@ import '../../app/core/models/client_model.dart';
 import '../../app/core/services/clients_service.dart';
 
 class ClientsViewModel extends BaseViewModel {
+  //se almacena el token
   final AuthService _authService = GetIt.instance<AuthService>();
 
   final ClientsService _clientsService = GetIt.instance<ClientsService>();
+
+  int _loadFiveClients = 0;
+
+  get loadFiveClients => _loadFiveClients;
+
+  void loadFiveClientsMore() {
+    if (_loadFiveClients < _filteredClients.length) {
+      _loadFiveClients =
+          (_loadFiveClients + 5).clamp(0, _filteredClients.length);
+      notifyListeners();
+    }
+  }
 
   List<Client> _clients = [];
   List<Client> get clients => _clients;
@@ -19,12 +32,45 @@ class ClientsViewModel extends BaseViewModel {
     setBusy(true);
     try {
       _clients = await _clientsService.fetchClients(_authService.token!);
-      print(_clients);
+      _filteredClients = _clients;
+      _loadFiveClients = 5;
+
       _errorMessage = null;
     } catch (e) {
       _errorMessage = 'Failed to load clients';
     }
     setBusy(false);
+    notifyListeners();
+  }
+
+  List<Client> _filteredClients = [];
+
+  List<Client> get displayedClients =>
+      _filteredClients.take(_loadFiveClients).toList();
+
+  void searchClients(String query) {
+    // Limpia el `query` eliminando espacios extras
+    final searchQuery = query.trim().toLowerCase();
+
+    // Si el query está vacío, muestra todos los clientes
+    if (searchQuery.isEmpty) {
+      _filteredClients = List.from(_clients);
+    } else {
+      _filteredClients = _clients.where((client) {
+        // Concatena firstname y lastname, y compara con el email
+        final name =
+            '${client.firstname ?? ''} ${client.lastname ?? ''}'.toLowerCase();
+        final email = client.email?.toLowerCase() ?? '';
+
+        // Verifica si el query aparece en el nombre o email
+        return name.contains(searchQuery) || email.contains(searchQuery);
+      }).toList();
+    }
+
+    // Reinicia el contador de clientes visibles para la lista filtrada
+    _loadFiveClients =
+        (_filteredClients.length >= 5) ? 5 : _filteredClients.length;
+
     notifyListeners();
   }
 }
