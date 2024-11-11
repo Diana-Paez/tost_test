a34d28d5a008e6b433b73c64f365c0557e454be8
Diana Paez <developermerci@gmail.com>
2024-11-10 19:44:43 -0500

    Add method to upload profile picture from local

--- lib/ui/viewmodels/client_detail_viewmodel.dart
+++ lib/ui/viewmodels/client_detail_viewmodel.dart
@@ -1,19 +1,38 @@
 import 'dart:developer';
+import 'dart:io';
 
+import 'package:flutter_test_tots/app/app.locator.dart';
+import 'package:flutter_test_tots/app/core/services/cloudinay_service.dart';
 import 'package:get_it/get_it.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:stacked/stacked.dart';
 
 import 'package:flutter_test_tots/app/core/services/auth_service.dart';
 import 'package:flutter_test_tots/app/core/services/clients_service.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 class ClientDetailViewModel extends BaseViewModel {
   final AuthService _authService = GetIt.instance<AuthService>();
   final ClientsService _clientsService = GetIt.instance<ClientsService>();
+  final CloudinaryService _cloudinaryService = locator<CloudinaryService>();
+  final NavigationService _navigationService =
+      GetIt.instance<NavigationService>();
+
+  final ImagePicker _picker = ImagePicker();
+  String? imageUrl;
+  File? _photoGalery;
 
   String _firstName = '';
   String _lastName = '';
   String _email = '';
   String? _photo;
+
+  File? get photoGalery => _photoGalery;
+
+  set photoGalery(File? value) {
+    _photoGalery = value;
+    notifyListeners();
+  }
 
   void setFirstName(String value) {
     _firstName = value;
@@ -22,6 +41,19 @@ class ClientDetailViewModel extends BaseViewModel {
 
   void setLastName(String value) {
     _lastName = value;
+    notifyListeners();
+  }
+
+  void setAll({
+    required String firstName,
+    required String lastName,
+    required String email,
+    required String photo,
+  }) {
+    _firstName = firstName;
+    _lastName = lastName;
+    _photo = photo;
+    _email = email;
     notifyListeners();
   }
 
@@ -46,13 +78,17 @@ class ClientDetailViewModel extends BaseViewModel {
   Future<bool?> saveClient() async {
     setBusy(true);
     try {
+      if (photoGalery != null) {
+        await uploadImage(photoGalery!);
+      }
       await _clientsService.requestServer(
           token: _authService.token!,
           firstName: _firstName,
           lastName: _lastName,
           email: _email,
-          photo: _photo,
+          photo: imageUrl ?? _photo,
           clientMethod: ClientMethod.create);
+      _navigationService.back(result: true);
 
       return true;
     } catch (e) {
@@ -63,7 +99,7 @@ class ClientDetailViewModel extends BaseViewModel {
     }
   }
 
-  Future<bool?> updateClient() async {
+  Future<bool?> updateClient(String id) async {
     setBusy(true);
 
     try {
@@ -73,7 +109,10 @@ class ClientDetailViewModel extends BaseViewModel {
           lastName: _lastName,
           email: _email,
           photo: _photo,
+          id: id,
           clientMethod: ClientMethod.update);
+      _navigationService.back(result: true);
+
       return true;
     } catch (e) {
       log('Error al guardar el cliente: $e');
@@ -87,12 +126,36 @@ class ClientDetailViewModel extends BaseViewModel {
     setBusy(true);
 
     try {
-      await _clientsService.deleteClient(_authService.token!, id);
+      await _clientsService.requestServer(
+          token: _authService.token!,
+          id: id,
+          clientMethod: ClientMethod.delete);
+      _navigationService.back(result: true);
 
       return true;
     } catch (e) {
       log('Error al guardar el cliente: $e');
       throw Exception('Failed to add client');
+    } finally {
+      setBusy(false);
+    }
+  }
+
+  Future<void> selectImage() async {
+    final XFile? pickedFile =
+        await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      photoGalery = File(pickedFile.path);
+    }
+  }
+
+  Future<void> uploadImage(File imageFile) async {
+    setBusy(true);
+    try {
+      imageUrl = await _cloudinaryService.uploadImage(imageFile);
+      notifyListeners();
+    } catch (e) {
+      log("Error uploading image: $e");
     } finally {
       setBusy(false);
     }
